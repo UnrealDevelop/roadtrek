21ca48ad795e914ef37a82048fbcd400be1e7671
zkirk <zhar.kirk@gmail.com>
2021-05-04 18:52:24 +0300

    first commit

---
+++ Source/roadtrek/roadtrekGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "roadtrekGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROADTREK_API AroadtrekGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
